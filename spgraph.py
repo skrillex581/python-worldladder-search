d10d30ee0a6afa070fca06e7d4c7b2429d4b0657
Zahir <skrillex581@users.noreply.github.com>
2016-11-16 19:15:51 +0200

    Update spgraph.py

--- spgraph.py
+++ spgraph.py
@@ -64,30 +64,10 @@ class Graph:
                     found[(i,j)]=True
         s += "}"
         return s
-    @staticmethod
-    def GetRandomGraph(maxnodes=30, maxweight=20):
-        m = Graph() #the graph is undirected (edges are added in dual)
-        map(m.AddVertex,range(1,maxnodes))
-        for i in range(1,maxnodes):
-            source = m.getRandomVertexId(maxnodes)
-            dest = m.getRandomVertexId(maxnodes)
-            if source <> dest:
-                 m.AddEdge(source,dest,randint(1,maxweight))
-        for j in [x for x in m._vertexes if len(m[x])==0]:
-            del m._vertexes[j]
-        return m
     def writeDotFile(self, fileName="graph.gvz"):
         f = open(fileName,'w')
         f.writelines(self.getDot())
-        f.close()
-    def getRandomVertexId(self,maxnodes):
-        d = randint(1,maxnodes)
-        while d+1 > len(self._vertexes.items()):
-            return self.getRandomVertexId(maxnodes)
-        j = self._vertexes.items()[d][0]
-        while j == None:
-            return self.getRandomVertex(maxnodes)
-        return j
+        f.close()  
     def FindShortestPath(self,source,destination=None):
         _pq = SOPriorityQueue()
         for v in self._vertexes:
@@ -113,31 +93,4 @@ class Graph:
                  if e==destination:
                      return self.GetShortestPath(destination)
         return self._distance
-def getBlogGraph():
-    m = Graph() #the graph is undirected (edges are added in dual)
-    map(m.AddVertex,[13,2,12,14,5,7,8,10])
-    m.AddEdge(13,14,2)
-    m.AddEdge(13,2,3)
-    m.AddEdge(2,12,6)
-    m.AddEdge(2,5,4)
-    m.AddEdge(14,12,4)
-    m.AddEdge(14,8,6)
-    m.AddEdge(7,12,10)
-    m.AddEdge(5,7,7)
-    m.AddEdge(2,2,4)
-    m.AddEdge(7,8,1)
-    m.AddEdge(7,10,6)
-    m.AddEdge(10,14,11)
-    return m
-def main():
-    #maxnodes =15
-    #maxweight = 10
-    #j = Graph.GetRandomGraph(maxnodes,maxweight)
-    #print j.FindShortestPath(j.getRandomVertexId(maxnodes),j.getRandomVertexId(maxnodes))
-    #print j.GetShortestPath(j.getRandomVertexId(maxnodes))
-    #print j
-    b = getBlogGraph()
-    print b.FindShortestPath(5,14)
-    pass
-if __name__ == '__main__':
-    main()
+
